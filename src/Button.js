c375f8bcfff610a70012269e8270ed65ac9bd738
Lidia Peleja <lidiapeleja@gmail.com>
2022-01-29 18:03:21 +0100

    first commit

---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+class ButtonPrimary extends React.Component {
+  render() {
+    return <Button>Hola</Button>;
+  }
+}
+
+export default ButtonPrimary;
